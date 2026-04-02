95e88242cdea9644ac4995491784230fa3084e93
Rajan Lama <rajan.lama@fusemachines.com>
2026-04-02 23:08:29 +0545

    removed unused blocs

--- src/blocks copy/icon/icons.js
+++
@@ -1,8 +0,0 @@
-// icons.js
-import { faUser, faStar, faHeart } from '@fortawesome/free-solid-svg-icons';
-
-export const ICONS = [
-  { name: 'user', icon: faUser },
-  { name: 'star', icon: faStar },
-  { name: 'heart', icon: faHeart },
-];
--- src/blocks copy/icon/save.js
+++
@@ -1,9 +0,0 @@
-export default function save({ attributes }) {
-  const { icon } = attributes;
-
-  return (
-    <div {...useBlockProps.save()}>
-      <span data-icon={icon}></span>
-    </div>
-  );
-}
